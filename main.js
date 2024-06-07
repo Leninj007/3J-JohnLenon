e2cad88dc41d702dbbb17323ea90d4b46891598f
Leninj007 <108944939+Leninj007@users.noreply.github.com>
2024-06-07 09:17:33 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-const botoes = document.querySelectorAll(".botao");
-console.log(botoes)
+const botoes  = document.querySelectorAll(".botao");
+const conteudo  = document.querySelectorAll(".aba-conteudo");
 for (let i = 0; i < botoes.length ; i++){
     botoes[i].onclick = function(){
-        for (j=0; j < botoes.length; j++){
-            botoes[j].classList.remove("ativo");
-        }
-        botoes[i].classList.add("ativo")
+    for (j=0;j<botoes.length;j++){
+        botoes[j].classList.remove("ativo");
+        conteudo[j].classList.remove("ativo")
     }
+    botoes[i].classList.add("ativo")
+    conteudo[i].classList.add("ativo")
+  }
 }
-
