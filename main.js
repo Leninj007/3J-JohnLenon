d55c6bb12490bc9957777bbd2112ae73b474e0ea
Leninj007 <108944939+Leninj007@users.noreply.github.com>
2024-06-20 07:53:33 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,10 +2,22 @@ const botoes  = document.querySelectorAll(".botao");
 const conteudo  = document.querySelectorAll(".aba-conteudo");
 const tempo = document.querySelectorAll(".contador");
  tempo[0]. textContent = "Olá";
-const tempoObjetivo1 = new Date(2007,06);
-const tempoObjetivo2 = new Date(2024,06);
+const tempoObjetivo1 = new Date(2024, 7,29);
+const tempoObjetivo2 = new Date(2024, 8,29);
+const tempoObjetivo3 = new Date(2024, 9,29);
+const tempoObjetivo4 = new Date(2024, 10,29);
+let segundos;
+let minutos;
+let horas;
+let dias;
 const agora = new Date();
-tempo[1].  textContent = agora
+tempo[0].  textContent = tempoObjetivo1 - agora;
+
+segundos = (tempoObjetivo1 - agora)/1000;
+minutos = segundos/60;
+horas = minutos/60;
+dias = horas/24;
+
 for (let i = 0; i < botoes.length ; i++){
     botoes[i].onclick = function(){
     for (j=0;j<botoes.length;j++){
