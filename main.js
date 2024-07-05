b180ee157efe4eddf144adba904fd386a60210f1
Leninj007 <108944939+Leninj007@users.noreply.github.com>
2024-07-05 09:46:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,29 @@
 const botoes  = document.querySelectorAll(".botao");
-const conteudo  = document.querySelectorAll(".aba-conteudo");
+const conteudo = document.querySelectorAll(".abas-conteudo");
 const tempo = document.querySelectorAll(".contador");
- tempo[0]. textContent = "Olá";
-const tempoObjetivo1 = new Date(2025,06);
-const tempoObjetivo2 = new Date(2024,06);
+const tempoObjetivo1 = new Date(2024,11,24,23,59);
+const tempoObjetivo2 = new Date(2024,9,28,23,59);
+const tempoObjetivo3 = new Date(2025,0,1,23,59);
+const tempoObjetivo4 = new Date(2025,2,14,23,59);
 const agora = new Date();
-let segundos ;
-segundos = (tempoObjetivo1 - agora)/1000;
-let minutos;
-minutos = segundos/60;
-let horas;
-horas = minutos/60
-let dias;
-dias = horas/24
-segundos = Math.floor(segundos);
-minutos = Math.floor(minutos);
-horas = Math.floor(horas);
-dias = Math.floor(dias);
-tempo[1].  textContent = `faltam ${dias} dias, ${horas} horas, ${minutos} minutos, ${segundos} segundos.`;
-for (let i = 0; i < botoes.length ; i++){
+let segundos = Math.floor((tempoObjetivo1-agora)/1000)
+let minutos = Math.floor(segundos/60)
+let horas = Math.floor(minutos/60)
+let dias = Math.floor(horas/24)
+
+segundos = segundos%60
+minutos = minutos%60
+horas = horas%24
+
+tempo[0].textContent = `Faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos`
+console.log(tempo);
+for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function(){
-    for (j=0;j<botoes.length;j++){
-        botoes[j].classList.remove("ativo");
-        conteudo[j].classList.remove("ativo")
+        for (let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+            conteudo[j].classList.remove("ativo")
+        }
+        botoes[i].classList.add("ativo ")
+        conteudo[i].classList.add("ativo")
     }
-    botoes[i].classList.add("ativo")
-    conteudo[i].classList.add("ativo")
-  }
 }
