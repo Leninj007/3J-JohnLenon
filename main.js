7c6a57f0557285f6ae684b40e9e722ef41642755
Leninj007 <108944939+Leninj007@users.noreply.github.com>
2024-07-25 07:58:10 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,17 +5,31 @@ const tempoObjetivo1 = new Date(2024,11,24,23,59);
 const tempoObjetivo2 = new Date(2024,9,28,23,59);
 const tempoObjetivo3 = new Date(2025,0,1,23,59);
 const tempoObjetivo4 = new Date(2025,2,14,23,59);
+
+function calculaTempo(tempoObjetivo){
 const agora = new Date();
-let segundos = Math.floor((tempoObjetivo1-agora)/1000)
-let minutos = Math.floor(segundos/60)
-let horas = Math.floor(minutos/60)
-let dias = Math.floor(horas/24)
+let segundos
+let minutos
+let horas
+let dias
+
+segundos = Math.floor((tempoObjetivo-agora)/1000)
+minutos = Math.floor(segundos/60)
+horas = Math.floor(minutos/60)
+dias = Math.floor(horas/24)
 
 segundos = segundos%60
 minutos = minutos%60
 horas = horas%24
 
-tempo[0].textContent = `Faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos`
+return `Faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos`
+}
+
+tempo[0].textContent = calculaTempo(tempoObjetivo1)
+tempo[1].textContent = calculaTempo(tempoObjetivo2)
+tempo[2].textContent = calculaTempo(tempoObjetivo3)
+tempo[3].textContent = calculaTempo(tempoObjetivo4)
+
 console.log(tempo);
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function(){
@@ -23,7 +37,7 @@ for (let i = 0; i < botoes.length; i++) {
             botoes[j].classList.remove("ativo");
             conteudo[j].classList.remove("ativo")
         }
-        botoes[i].classList.add("ativo ")
+        botoes[i].classList.add("ativo")
         conteudo[i].classList.add("ativo")
     }
 }
